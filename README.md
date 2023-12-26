20d05c5ae1b34b9df4ce53450d0180253d2eb7d4
Graham Trott <graham@computec.consulting>
2023-12-26 13:29:08 +0000

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,48 @@
+My partner takes a lot of photos. Since smartphones replaced compact cameras for taking photos, everything has been shovelled into Google Photos, but that still left her wit thousands of images distributed in a messy filing system with hundreds of folders and no real organization. She owned several computers over that period and each one had a backup of the previous one, so duplication is rife. Everything is in there somewhere, but how does she find it?
 
+There are two ways to deal with the problem. One is to find a tool that will do the job; the other is to write one. The problem with the first of these is that it will take time to find such a tool, largely because I can't figure a suitable question to ask. Having found a tool I then have to learn how to use it. Unless it's top-drawer commercial software it will lack a comprehensive manual, so this part could take hours of not days. Then there's finally the good chance that it won't actually do the job after all, so the whole exercise is wasted. And if the original search came up with several products, everything has to be repeated for each one.
+
+So that leaves writing my own. It's the Christmas break right now, so what better time, with none of the usual work pressures. I decided to write the thing in command-line Python; you'll find the code in the EasyCoder repository at http://xxxxxxx. By the time you read this the repo version may well have moved on, so the documentation below may well be out of date.
+
+Running the tool with no arguments outputs some basic help, as follows:
+```
+A tool to list extensions, copy/delete all files of a given type, remove duplicates etc.
+Syntax:
+pruner.py [options] [path]
+Options:
+   -mode types -- list all extensions
+   -mode listdup  -- list all duplicates with the given extension
+   -mode deldup -- delete all duplicates with the given extension
+   -mode copy -- copy files to target directory
+   -mode move -- move files to target directory
+   -ext -- specify an extension
+   -target [path] -- specify the target directory
+   -flatten -- don't maintain directory structure in copy/move
+   -ignore -- ignore duplicates in copy/move
+   -help -- this help message
+```
+The tool walks any given directory recursively, taking appropriate actions as it goes. Here are examples of how to use each option (at the time of writing). In each case, `<path>` is the path (absolute or relative) to the directory bing scanned:
+```
+pruner.py -mode types <path>
+```
+Output a list of all the file types (extensions) found in the directory and its subdirectories, with the number of files of that type, sorted most to least.
+```
+pruner.py -mode listdup -ext jpg <path>
+```
+List all duplicate file names. The first one encountered is take to be the original and all others duplicates. The fact that tne names are the same doesn't necessarily mean they are the same file, of course, so if there's any doubt you'll need to examine the output carefully.
+```
+pruner.py -mode deldup -ext png <path>
+```
+Delete all duplicate files (files with the same name but in different directories). Use `listdup` first to check these really are duplicates.
+```
+pruner.py -mode copy -ext doc -target <target folder> <path>
+```
+Copy all files with the given extension, putting them into `target folder`. The same folder hierarchy will be maintained, or you can add the `-flatten` option to force everything to go into a single directory.
+```
+pruner.py -mode move -ext doc -target <target folder> <path>
+```
+move all files with the given extension, putting them into `target folder`. This is the same as `copy` but the copied files will be deleted as they are moved.
+
+With both `copy` and `move` you can also add the `-ignore` option to ignore duplicates.
+
+After I ran the `copy` once for each wanted file type, the extracted files took up just offer 100GB, compared to over 500GB on the original backup disk. Most of the rest is old emails, ancient Windows executables and so on, none of which need to be kept, and it should now be a lot easier for my partner to find photos, videos or documents should the need arise. I hope others here may also find it useful in pruning the immense piles of clutter we fill our computers with. My next job is to check if a duplicate file name really means an identical file, but if someone else gets there before me I'm happy to use their code.
---
+++ unpick.py
@@ -0,0 +1,318 @@
+#! /usr/bin/python3
+
+# unpick.py
+
+import sys, os, pathlib, shutil
+
+extensions = dict()
+files = dict()
+duplicates = dict()
+option_extension = None
+option_target = None
+option_types = False
+option_list = False
+option_duplicates = False
+option_copy = False
+option_move = False
+option_delete = False
+option_remove = False
+option_flatten = False
+option_ignore = False
+option_help = False
+path = None
+
+###############################################################################
+# List all the different extensions for files under the given path
+def getExtensions(path):
+    global extensions
+    for f in os.listdir(path):
+        if f[0] != '.':
+            p = os.path.join(path, f)
+            if os.path.isdir(p):
+                getExtensions(p)
+            else:
+                parts = p.split('.')
+                if len(parts) > 1:
+                    ext = parts[len(parts) - 1].lower()
+                    if len(ext.split('/')) == 1 and not ext.isnumeric():
+                        if ext in extensions:
+                            extensions[ext] = extensions[ext] + 1
+                        else:
+                            extensions[ext] = 1
+
+###############################################################################
+# List all files with a given extension
+def listFiles(path):
+    global extension, files, delete
+    for f in os.listdir(path):
+        if f[0] != '.':
+            p = os.path.join(path, f)
+            if os.path.isdir(p):
+                listFiles(p)
+                if len(os.listdir(path)) == 0:
+                    pathlib.Path.rmdir(path)
+            else:
+                parts = p.split('.')
+                if len(parts) > 0:
+                    ext = parts[len(parts) - 1].lower()
+                    if ext == option_extension:
+                        print(p)
+
+###############################################################################
+# List all duplicate files with a given extension
+def listDuplicates(path):
+    global extension, files, delete
+    for f in os.listdir(path):
+        if f[0] != '.':
+            p = os.path.join(path, f)
+            if os.path.isdir(p):
+                listDuplicates(p)
+                if len(os.listdir(path)) == 0:
+                    pathlib.Path.rmdir(path)
+            else:
+                parts = p.split('.')
+                if len(parts) > 0:
+                    ext = parts[len(parts) - 1].lower()
+                    if ext == option_extension:
+                        if f in files:
+                            print(f'Original: {files[f]}')
+                            print(f'Duplicate: {p}')
+                        else:
+                            files[f] = p
+
+###############################################################################
+# Copy a file to the target directory
+def copyFile(f):
+    global option_target
+    parts = f.split('/')
+    if option_flatten:
+        if not os.path.exists(option_target):
+            os.makedirs(option_target)
+        cf = f'{option_target}/{parts[-1]}'
+        print(f'Copy {f} to {cf}')
+        shutil.copyfile(f, cf)
+    else:
+        cf = f'{option_target}/{f}'
+        dirs = f'{option_target}/{"/".join(parts[:-1])}'
+        if not os.path.exists(dirs):
+            os.makedirs(dirs)
+        print(f'Copy {f}\nto {cf.replace("//", "/")}')
+        shutil.copyfile(f, cf)
+
+# Copy all files with a given extension
+def copy(path):
+    global extension, files, ignore
+    for f in os.listdir(path):
+        if f[0] != '.':
+            p = os.path.join(path, f)
+            if os.path.isdir(p):
+                copy(p)
+            else:
+                parts = p.split('.')
+                if len(parts) > 0:
+                    ext = parts[len(parts) - 1].lower()
+                    if ext == option_extension:
+                        if f in files:
+                            if not option_ignore:
+                                copyFile(p)
+                        else:
+                            files[f] = True
+                            copyFile(p)
+
+###############################################################################
+# Move all files with a given extension
+def move(path):
+    global extension, files, target, ignore
+    for f in os.listdir(path):
+        if f[0] != '.':
+            p = os.path.join(path, f)
+            if os.path.isdir(p):
+                move(p)
+            else:
+                parts = p.split('.')
+                if len(parts) > 0:
+                    ext = parts[len(parts) - 1].lower()
+                    if ext == option_extension:
+                        if f in files:
+                            if not option_ignore:
+                                copyFile(p)
+                            os.unlink(p)
+                        else:
+                            files[f] = True
+                            copyFile(p)
+                            os.unlink(p)
+
+###############################################################################
+# Delete all files with a given extension
+def deleteFiles(path):
+    global extension, files, delete
+    for f in os.listdir(path):
+        if f[0] != '.':
+            p = os.path.join(path, f)
+            if os.path.isdir(p):
+                deleteDuplicates(p)
+                if len(os.listdir(path)) == 0:
+                    pathlib.Path.rmdir(path)
+            else:
+                parts = p.split('.')
+                if len(parts) > 0:
+                    ext = parts[len(parts) - 1].lower()
+                    if ext == option_extension:
+                        print(f'Delete {p}')
+                        os.unlink(p)
+
+###############################################################################
+# Delete all duplicate files with a given extension
+def deleteDuplicates(path):
+    global extension, files, delete
+    for f in os.listdir(path):
+        if f[0] != '.':
+            p = os.path.join(path, f)
+            if os.path.isdir(p):
+                deleteDuplicates(p)
+                if len(os.listdir(path)) == 0:
+                    pathlib.Path.rmdir(path)
+            else:
+                parts = p.split('.')
+                if len(parts) > 0:
+                    ext = parts[len(parts) - 1].lower()
+                    if ext == option_extension:
+                        if f in files:
+                            print(f'Delete {p}')
+                            os.unlink(p)
+                        else:
+                            files[f] = True
+
+###############################################################################
+def showHelp():
+    print('A tool to list extensions, copy/delete all files of a given type, remove duplicates etc.')
+    print('Syntax:\nunpick.py [options] [path]')
+    print('Options (note that many of these are mutually exclusive):')
+    print('   -types, -extensions, -ext -- list all file extensions')
+    print('   -list  -- list all files with the given extension')
+    print('   -duplicates, -dup  -- list all duplicates with the given extension')
+    print('   -delete, -del -- delete all files with the given extension')
+    print('   -remove, -rem -- delete all duplicates with the given extension')
+    print('   -copy -- copy files to target directory')
+    print('   -move -- move files to target directory')
+    print('   -type, -extension, -ext -- specify an extension')
+    print('   -target [path] -- copy/move to this target directory')
+    print('   -flatten -- don\'t maintain directory structure in copy/move')
+    print('   -ignore -- ignore duplicates in copy/move')
+    print('   -help -- this help message')
+
+###############################################################################
+###############################################################################
+# Program starts here
+# Scan the command-line arguments
+if len(sys.argv) == 1:
+    option_help = True
+else:
+    i = 1
+    while i < len(sys.argv):
+        arg = sys.argv[i]
+        if arg[0] == '-':
+            option = arg[1:]
+            if option == 'types' or option == 'extensions' or option == 'ext':
+                option_types = True
+            elif option == 'type' or option == 'extension' or option == 'ext':
+                i = i + 1
+                option_extension = sys.argv[i]
+            elif option == 'target':
+                i = i + 1
+                option_target = sys.argv[i]
+            elif option == 'list':
+                option_list = True
+            elif option == 'duplicates' or option == 'dup':
+                option_duplicates = True
+            elif option == 'copy':
+                option_copy = True
+            elif option == 'move':
+                option_move = True
+            elif option == 'delete' or option == 'del':
+                option_delete = True
+            elif option == 'remove' or option == 'rem':
+                option_remove = True
+            elif option == 'flatten':
+                option_flatten = True
+            elif option == 'ignore':
+                option_ignore = True
+            elif option == 'help':
+                option_help = True
+        else:
+            path = arg
+        i = i + 1
+
+###############################################################################
+# Perform the requested operation
+if option_help:
+    showHelp()
+elif not option_types and not option_list and not option_duplicates and not option_delete and not option_remove and not option_copy and not option_move:
+    print('No action (types, list, duplicates, delete, remove, copy or move) has been specified')
+else:
+    if option_types:
+        if path == None:
+            print('No path has been specified')
+        else:
+            getExtensions(path)
+            tmp = []
+            for ext, count in extensions.items():
+                tmp.append((count, ext))
+            tmp = sorted(tmp, reverse=True)
+            for (count, ext) in tmp:
+                print(f'{ext} {count}')
+
+    elif option_list:
+        if option_extension == None:
+            print('No extension has been specified')
+        else:
+            listFiles(arg)
+
+    elif option_duplicates:
+        if option_extension == None:
+            print('No extension has been specified')
+        else:
+            listDuplicates(arg)
+
+    elif option_copy:
+        if option_extension == None:
+            print('No extension has been specified')
+        elif option_target == None:
+            print('No target directory has been specified')
+        elif path == None:
+            print('No path has been specified to copy from')
+        else:
+            target = f'{option_target}/{option_extension}'
+            print(f'Copy from {path} to {target}')
+            copy(path)
+
+    elif option_move:
+        if option_extension == None:
+            print('No extension has been specified')
+        elif option_target == None:
+            print('No target directory has been specified')
+        elif path == None:
+            print('No path has been specified to move from')
+        else:
+            target = f'{option_target}/{option_extension}'
+            print(f'Move from {path} to {target}')
+            move(path)
+
+    elif option_delete:
+        if option_extension == None:
+            print('No extension has been specified')
+        elif path == None:
+            print('No path has been specified')
+        else:
+            deleteFiles(path)
+
+    elif option_remove:
+        if option_extension == None:
+            print('No extension has been specified')
+        elif path == None:
+            print('No path has been specified')
+        else:
+            deleteDuplicates(path)
+
+    elif option_help:
+        showHelp()
